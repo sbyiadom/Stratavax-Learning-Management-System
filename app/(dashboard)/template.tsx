d58017fa1fb24c71b8c2f43bc26669ad9395f637
sbyiadom <sbyiadom88@gmail.com>
2026-02-28 20:16:20 +0000

    Delete app/(dashboard) directory

--- app/(dashboard)/template.tsx
+++
@@ -1,7 +0,0 @@
-'use client'
-
-// Simple template to help Next.js generate client manifest
-// No external dependencies required
-export default function DashboardTemplate({ children }: { children: React.ReactNode }) {
-  return <>{children}</>
-}
