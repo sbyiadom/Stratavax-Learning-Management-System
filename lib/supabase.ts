f2f7aad1df1896e70ef312d311116e94ec9eb7c3
sbyiadom <sbyiadom88@gmail.com>
2026-03-20 21:51:55 +0000

    Delete lib directory

--- lib/supabase.ts
+++
@@ -1,8 +0,0 @@
-import { createBrowserClient } from '@supabase/ssr'
-
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL_NEW!
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY_NEW!
-
-export const createClient = () => {
-  return createBrowserClient(supabaseUrl, supabaseAnonKey)
-}
